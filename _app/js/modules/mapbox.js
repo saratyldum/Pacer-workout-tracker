6ea246285186a83a865de570bfecdee733b1c42a
saratyldum <98975412+saratyldum@users.noreply.github.com>
2023-04-20 11:06:04 +0200

    add marker and popup on form submit

--- _app/js/modules/mapbox.js
+++ _app/js/modules/mapbox.js
@@ -7,40 +7,46 @@ export default function mapbox(userCoordinates) {
 	const inputCadence = document.querySelector('.workout-form__input--cadence');
 	const inputElevation = document.querySelector('.workout-form__input--elevation');
 
+	let map, mapEvent;
+
 	/**
 	 * @TODO hide access token
 	 */
 	mapboxgl.accessToken = 'pk.eyJ1IjoidHlsc2EiLCJhIjoiY2xnbmppcndqMDBzazNkcGVld2gxcmVqMyJ9.y_xYrcqqEf_CD9mfOzHghg	';
 	
-	const map = new mapboxgl.Map({
-		container: 'map', // container ID
-		style: 'mapbox://styles/mapbox/streets-v12', // style URL
-		center: userCoordinates, // starting position [lng, lat]
-		zoom: 13, // starting zoom
-		});
+	map = new mapboxgl.Map({
+	container: 'map', // container ID
+	style: 'mapbox://styles/mapbox/streets-v12', // style URL
+	center: userCoordinates, // starting position [lng, lat]
+	zoom: 13, // starting zoom
+	});
 
-		map.on('click', handleMapClick);
+	map.on('click', handleMapClick);
+	form.addEventListener('submit', handleFormSubmit);
 
-		function handleMapClick(event) {
-			form.classList.remove('hidden');
-			inputDistance.focus();
-			// const latitude = event.lngLat.lat;
-			// const longitude = event.lngLat.lng;
-			// const coordinates = [longitude, latitude]
+	function handleMapClick(event) {
+		mapEvent = event;
+		form.classList.remove('hidden');
+		inputDistance.focus();
+	}
 
-			// addFeatures(coordinates)
-		}
 
-		function addFeatures(coordinates) {
-			const popup = new mapboxgl.Popup({closeOnClick: false})
-			.setText('workout')
-			.setLngLat(coordinates)
-			.addClassName('running-popup');
+	function handleFormSubmit(event) {
+		event.preventDefault();
+		const latitude = mapEvent.lngLat.lat;
+		const longitude = mapEvent.lngLat.lng;
+		const coordinates = [longitude, latitude]
 
-			const marker = new mapboxgl.Marker({ color: 'var(--primary-color)'})
-			.setLngLat(coordinates)
-			.setPopup(popup)
-			.addTo(map)
-		}
+		const popup = new mapboxgl.Popup({closeOnClick: false})
+		.setText('workout')
+		.setLngLat(coordinates)
+		.addClassName('running-popup');
 
+		const marker = new mapboxgl.Marker({ color: 'var(--primary-color)'})
+		.setLngLat(coordinates)
+		.setPopup(popup)
+		.addTo(map)
+	}
+
+		
 }
