277fad63aef85b5f25aeae13767105653b9feb0d
saratyldum <98975412+saratyldum@users.noreply.github.com>
2023-04-20 10:52:19 +0200

    selected form elements from html

--- _app/js/modules/workoutForm.js
+++ _app/js/modules/workoutForm.js
@@ -1,3 +1,11 @@
-export default workoutForm() {
-	
+export default function workoutForm() {
+
+	const form = document.querySelector('.workout-form');
+	const containerWorkouts = document.querySelector('.workouts');
+	const inputType = document.querySelector('.workout-form__input--type');
+	const inputDistance = document.querySelector('.workout-form__input--distance');
+	const inputDuration = document.querySelector('.workout-form__input--duration');
+	const inputCadence = document.querySelector('.workout-form__input--cadence');
+	const inputElevation = document.querySelector('.workout-form__input--elevation');
+
 }
