5229694ca3b7c4610ad18002ef9b7c952d680424
saratyldum <98975412+saratyldum@users.noreply.github.com>
2023-04-20 11:58:51 +0200

    started implementing structure for data handling

--- _app/js/modules/workoutForm.js
+++ _app/js/modules/workoutForm.js
@@ -8,7 +8,13 @@ export default function workoutForm() {
 	const inputCadence = document.querySelector('.workout-form__input--cadence');
 	const inputElevation = document.querySelector('.workout-form__input--elevation');
 
+	let type;
+	let coordinates; // [long, lat]
+	let distance; // in km
+	let duration; // in min
+
 	inputType.addEventListener('change', handleInputTypeChange);
+	
 
 	function handleInputTypeChange() {
 		toggleElevationField();
@@ -17,6 +23,41 @@ export default function workoutForm() {
 	function toggleElevationField() {
 		inputElevation.closest('.workout-form__row').classList.toggle('workout-form__row--hidden');
 		inputCadence.closest('.workout-form__row').classList.toggle('workout-form__row--hidden');
+	}
+
+	function workouts(coordinates, distance, duration) {
+		date = new Date();
+		id = (Date.now() + '').slice(-10);
+	}
+
+	function setDescription() {
+		const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+
+	}
+
+	function runningWorkout(coordinates, distance, duration, cadence) {
+		// const cadence = inputCadence.value;
+		const pace = calculatePace(duration, distance);
+	}
+
+	function cyclingWorkout(coordinates, distance, duration, elevation) {
+		// const elevation = inputElevation.value;
+		const speed = calculateSpeed(distance, duration)
+	}
+
+	function calculatePace(duration, distance) {
+		// min/km
+		const pace = duration / distance;
+
+		return pace;
+	}
+
+	function calculateSpeed(distance, duration) {
+		// km/h
+		const speed = distance / (duration / 60);
+
+		return speed;
 	}
 
 }
