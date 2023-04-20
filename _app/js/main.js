bf7c31adb6ab13523c994d9a89a07bceaacc2f0d
saratyldum <98975412+saratyldum@users.noreply.github.com>
2023-04-20 09:14:41 +0200

    created js module for getting user location

--- _app/js/main.js
+++ _app/js/main.js
@@ -1,3 +1,5 @@
 import mapbox from "./modules/mapbox.js";
+import userLocation from "./modules/userLocation.js"
 
 mapbox();
+userLocation();
---
+++ _app/js/modules/userLocation.js
@@ -0,0 +1,4 @@
+export default function userLocation() {
+
+	navigator.geolocation.getCurrentPosition()
+}
