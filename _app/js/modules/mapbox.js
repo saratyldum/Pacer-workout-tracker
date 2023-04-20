4d16cb0e5d742ecfc1c24979f0db7c9763573f25
saratyldum <98975412+saratyldum@users.noreply.github.com>
2023-04-20 10:55:33 +0200

    selected form elements from html

--- _app/js/modules/mapbox.js
+++ _app/js/modules/mapbox.js
@@ -1,4 +1,12 @@
 export default function mapbox(userCoordinates) {
+	const form = document.querySelector('.workout-form');
+	const containerWorkouts = document.querySelector('.workouts');
+	const inputType = document.querySelector('.workout-form__input--type');
+	const inputDistance = document.querySelector('.workout-form__input--distance');
+	const inputDuration = document.querySelector('.workout-form__input--duration');
+	const inputCadence = document.querySelector('.workout-form__input--cadence');
+	const inputElevation = document.querySelector('.workout-form__input--elevation');
+
 	/**
 	 * @TODO hide access token
 	 */
