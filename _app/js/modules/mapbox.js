e1a754b961425ac158212a4c5fe1397374eed203
saratyldum <98975412+saratyldum@users.noreply.github.com>
2023-04-20 09:40:13 +0200

    add marker on click

--- _app/js/modules/mapbox.js
+++ _app/js/modules/mapbox.js
@@ -3,14 +3,28 @@ export default function mapbox(userCoordinates) {
 	 * @TODO hide access token
 	 */
 	mapboxgl.accessToken = 'pk.eyJ1IjoidHlsc2EiLCJhIjoiY2xnbmppcndqMDBzazNkcGVld2gxcmVqMyJ9.y_xYrcqqEf_CD9mfOzHghg	';
+	
 	const map = new mapboxgl.Map({
-	container: 'map', // container ID
-	style: 'mapbox://styles/mapbox/streets-v12', // style URL
-	center: userCoordinates, // starting position [lng, lat]
-	zoom: 13, // starting zoom
-	});
+		container: 'map', // container ID
+		style: 'mapbox://styles/mapbox/streets-v12', // style URL
+		center: userCoordinates, // starting position [lng, lat]
+		zoom: 13, // starting zoom
+		});
 
-	const marker2 = new mapboxgl.Marker({ color: 'var(--primary-color)'})
-		.setLngLat(userCoordinates)
-		.addTo(map)
+		map.on('click', handleMapClick);
+
+		function handleMapClick(event) {
+			const latitude = event.lngLat.lat;
+			const longitude = event.lngLat.lng;
+			const markerCoordinates = [longitude, latitude]
+
+			addMarker(markerCoordinates)
+		}
+
+		function addMarker(markerCoordinates) {
+			const marker2 = new mapboxgl.Marker({ color: 'var(--primary-color)'})
+			.setLngLat(markerCoordinates)
+			.addTo(map)
+		}
+
 }
