c08950a617101f98e33b354a28cbb0345d5967b9
saratyldum <98975412+saratyldum@users.noreply.github.com>
2023-04-23 12:35:42 +0200

    added some todo comments

--- _app/js/main.js
+++ _app/js/main.js
@@ -5,5 +5,5 @@ import weeklyProgress from "./modules/weeklyProgress.js";
 
 // mapbox(); få denne til å kjøre herfra, ikke fra userlocation
 getUserPosition();
-workoutForm();
-weeklyProgress();
+workoutForm(); //return distance whenever a new workout is added
+weeklyProgress(); //use distance to add onto progress bar
