39d7af1750719f880f09daff081cb4d538f9c037
saratyldum <98975412+saratyldum@users.noreply.github.com>
2023-04-23 12:29:57 +0200

    styled progressBarWidth dynamcally based on input

--- _app/js/modules/weeklyProgress.js
+++ _app/js/modules/weeklyProgress.js
@@ -2,6 +2,25 @@ export default function weeklyProgress() {
 	const progressBar = document.querySelector('.weekly-goal__progress');
 	const goalInput = document.querySelector('.weekly-goal__input');
 	let progress = 0;
-	let finalValue = 0;
-	let max = 0;
+	let finalValue = 10; //dette tallet må oppdateres når treninger addes
+	let weeklyValue = 0;
+	let progressPerecentage;
+
+	goalInput.addEventListener('keyup', handleGoalInputKeyup);
+
+	function handleGoalInputKeyup() {
+		weeklyValue = parseInt(goalInput.value, 10);
+		changeProgressBarWidth();
+	}
+
+	function calculateProgressPercentage() {
+		progressPerecentage = (finalValue / weeklyValue) * 100;
+		return progressPerecentage;
+	}
+
+	function changeProgressBarWidth() {
+		const progressBarWidth = calculateProgressPercentage()
+		progressBar.style.width = `${progressBarWidth}%`;
+		console.log(progressBar);
+	}
 }
