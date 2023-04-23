b1e55cae0cf08e920db3ed3582585ea583a1cd95
saratyldum <98975412+saratyldum@users.noreply.github.com>
2023-04-23 12:18:47 +0200

    got elements to dynamically change weekly progress

--- _app/js/main.js
+++ _app/js/main.js
@@ -1,7 +1,9 @@
 import loadMap from "./modules/loadMap.js";
 import getUserPosition from "./modules/getUserPosition.js";
 import workoutForm from "./modules/workoutForm.js";
+import weeklyProgress from "./modules/weeklyProgress.js";
 
 // mapbox(); få denne til å kjøre herfra, ikke fra userlocation
 getUserPosition();
 workoutForm();
+weeklyProgress();
---
+++ _app/js/modules/weeklyProgress.js
@@ -0,0 +1,7 @@
+export default function weeklyProgress() {
+	const progressBar = document.querySelector('.weekly-goal__progress');
+	const goalInput = document.querySelector('.weekly-goal__input');
+	let progress = 0;
+	let finalValue = 0;
+	let max = 0;
+}
