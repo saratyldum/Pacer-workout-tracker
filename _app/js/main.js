8f84e793f9fa95c511cfe4288d84aa736ff5c920
saratyldum <98975412+saratyldum@users.noreply.github.com>
2023-04-26 13:52:31 +0200

    rendered workouts from sanity on map and in list

--- _app/js/main.js
+++ _app/js/main.js
@@ -1,13 +1,32 @@
 import loadMap from "./modules/loadMap.js";
 import fetchWorkouts from "./modules/fetchWorkouts.js";
-import getUserPosition from "./modules/getUserPosition.js";
+// import getUserPosition from "./modules/getUserPosition.js";
 import workoutForm from "./modules/workoutForm.js";
 import weeklyProgress from "./modules/weeklyProgress.js";
 import dailyProgress from "./modules/dailyProgress.js";
+import renderWorkouts from "./modules/renderWorkouts.js"
 
 // mapbox(); få denne til å kjøre herfra, ikke fra userlocation
+
+let map;
 const workouts = await fetchWorkouts();
-getUserPosition();
+
+if(navigator.geolocation)
+navigator.geolocation.getCurrentPosition(handleGeolocationSucess, errorPosition)
+
+function handleGeolocationSucess(position) {
+const map = loadMap(position)
+renderWorkouts(map, workouts);
+
+}
+
+function errorPosition() {
+alert('Could not get your position')
+}
+
+
+
+// getUserPosition();
 workoutForm(); //return distance whenever a new workout is added
 weeklyProgress(); //use distance to add onto progress bar
 dailyProgress();
