07d85d4b10a5b4cb974f88f6b6f8078e34c59ced
saratyldum <98975412+saratyldum@users.noreply.github.com>
2023-04-26 13:24:45 +0200

    fetched all workouts from sanity

--- _app/js/main.js
+++ _app/js/main.js
@@ -1,10 +1,12 @@
 import loadMap from "./modules/loadMap.js";
+import fetchWorkouts from "./modules/fetchWorkouts.js";
 import getUserPosition from "./modules/getUserPosition.js";
 import workoutForm from "./modules/workoutForm.js";
 import weeklyProgress from "./modules/weeklyProgress.js";
 import dailyProgress from "./modules/dailyProgress.js";
 
 // mapbox(); få denne til å kjøre herfra, ikke fra userlocation
+const workouts = await fetchWorkouts();
 getUserPosition();
 workoutForm(); //return distance whenever a new workout is added
 weeklyProgress(); //use distance to add onto progress bar
