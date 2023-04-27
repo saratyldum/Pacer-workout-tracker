244f8b39f1589edf21e84719cde1e48424989978
saratyldum <98975412+saratyldum@users.noreply.github.com>
2023-04-27 13:14:59 +0200

    updated weekly progress based on sanity value

--- _app/js/modules/weeklyProgress.js
+++ _app/js/modules/weeklyProgress.js
@@ -1,25 +1,114 @@
-export default function weeklyProgress() {
+import { sanity } from "../sanity.js";
+import { sanityMutate  } from "../sanity.js";
+
+export default  async function weeklyProgress() {
 	const progressBar = document.querySelector('.weekly-goal__progress-bar--bar');
-	const goalInput = document.querySelector('.weekly-goal__input');
+	const weeklyDistanceDone = document.querySelector('.weekly-goal__progress-info--done');
+	const weeklyDistanceRemaining = document.querySelector('.weekly-goal__progress-info--remaining');
+
+	let goalInput = document.querySelector('.weekly-goal__input');
 	let progress = 0;
-	let finalValue = 10; //dette tallet må oppdateres når treninger addes
+	let finalValue;
 	let weeklyValue = 0;
 	let progressPerecentage;
 
-	goalInput.addEventListener('keyup', handleGoalInputKeyup);
+	// goalInput.addEventListener('keyup', handleGoalInputKeyup);
 
-	function handleGoalInputKeyup() {
-		weeklyValue = parseInt(goalInput.value, 10);
-		changeProgressBarWidth();
+	const weeklyGoal = await setWeeklyGoal();
+	const workouts = await fetchDistance();
+	calculateDistance(workouts)
+	changeProgressBarWidth(weeklyGoal);
+
+
+	async function setWeeklyGoal() {
+		const weeklyGoal = await fetchWeeklyGoal();
+		goalInput.value = '';
+		goalInput.value = weeklyGoal;
+		return weeklyGoal;
 	}
 
-	function calculateProgressPercentage() {
-		progressPerecentage = (finalValue / weeklyValue) * 100;
+
+	async function fetchWeeklyGoal() {
+		const query = `*[_id == 'settings'][0]`
+		const goal = await sanity.fetch(query);
+		return goal.weeklyGoal;
+	}
+
+	async function fetchDistance() {
+		const query = `*[_type == 'workout']{
+			distance
+		}`;
+
+		const workouts = await sanity.fetch(query);
+
+		return workouts;
+	}
+
+	function calculateDistance(workouts) {
+		let distance = [];
+		for (const workout of workouts) {
+			distance.push(workout.distance)
+		}
+
+		const initialValue = 0;
+		
+		const reducedDistance = distance.reduce(
+			(accumulator, currentValue) => accumulator + currentValue, initialValue
+		);
+
+		finalValue = reducedDistance;
+		weeklyDistanceDone.textContent = reducedDistance;
+		console.log(weeklyGoal);
+		weeklyDistanceRemaining.textContent = parseInt((weeklyGoal - reducedDistance));
+	}
+
+
+	function handleGoalInputKeyup(event) {
+		console.log(event.key);
+		if(event.key == 'Enter') {
+			weeklyValue = parseInt(goalInput.value, 10);
+			console.log(weeklyValue);
+			changeProgressBarWidth();
+			sendWeeklyGoalToSanity(weeklyValue);
+			setWeeklyGoal();
+		} else {
+			return
+		}
+	}
+
+	function calculateProgressPercentage(weeklyGoal) {
+		progressPerecentage = (finalValue / weeklyGoal) * 100;
 		return progressPerecentage;
 	}
 
-	function changeProgressBarWidth() {
-		const progressBarWidth = calculateProgressPercentage()
+	function changeProgressBarWidth(weeklyGoal) {
+		const progressBarWidth = calculateProgressPercentage(weeklyGoal);
+		console.log(weeklyGoal);
 		progressBar.style.width = `${progressBarWidth}%`;
+	}
+
+	async function sendWeeklyGoalToSanity(goal) {
+		//FUNKER IKKE Å ENDRE??
+
+		try {
+			const mutations = [
+				{
+					'createOrReplace': {
+						_type: 'settings',
+						_id: 'settings',
+						weeklyGoal: `${goal}`
+					}
+				}
+			]
+
+			const params = {
+				dryRun: false
+			}
+
+			const result = await sanityMutate.mutate(mutations, params);
+
+		} catch(error) {
+			console.error(error.message);
+		}
 	}
 }
