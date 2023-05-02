47d868d7085c63f0e55d6335e1cf2d7357bbb795
saratyldum <98975412+saratyldum@users.noreply.github.com>
2023-05-02 11:32:09 +0200

    removed unused variables

--- _app/js/modules/workoutForm.js
+++ _app/js/modules/workoutForm.js
@@ -9,10 +9,6 @@ export default async function workoutForm(map) {
 	const inputCadence = document.querySelector('.workout-form__input--cadence');
 	const inputElevation = document.querySelector('.workout-form__input--elevation');
 
-	let type;
-	let coordinates; // [long, lat]
-	let distance; // in km
-	let duration; // in min
 	let mapEvent, description;
 
 	inputType.addEventListener('change', handleInputTypeChange);
@@ -47,7 +43,6 @@ export default async function workoutForm(map) {
 		mapEvent = event;
 		form.classList.remove('hidden');
 		inputDistance.focus();
-
 	}
 
 	function newWorkout(event) {
@@ -96,74 +91,6 @@ export default async function workoutForm(map) {
 		}
 	}
 
-	// Render workout on map as a marker
-	function renderWorkoutMaker(coordinates, description) {
-		//Create popup
-		const popup = new mapboxgl.Popup({closeOnClick: false})
-		.setText(description)
-		.setLngLat(coordinates)
-		.addClassName('running-popup');
-
-		//Add marker
-		const marker = new mapboxgl.Marker({ color: 'var(--primary-color)'})
-		.setLngLat(coordinates)
-		.setPopup(popup)
-		.addTo(map)
-	}
-
-	//Render wokrout in list
-	function renderWorkoutList(workout) {
-		//SKRIV OM!!!!
-		let html = `
-		<li class="workout workout--${workout.type}" data-id="${workout.id}">
-			<h2 class="workout__title">${workout.description}</h2>
-			<div class="workout__details">
-				<span class="workout__icon">${
-				workout.type === 'running' ? '🏃‍♂️' : '🚴‍♀️'
-				}</span>
-				<span class="workout__value">${workout.distance}</span>
-				<span class="workout__unit">km</span>
-			</div>
-			<div class="workout__details">
-				<span class="workout__icon">⏱</span>
-				<span class="workout__value">${workout.duration}</span>
-				<span class="workout__unit">min</span>
-			</div>
-		`;
-
-		if (workout.type === 'running')
-		html += `
-			<div class="workout__details">
-				<span class="workout__icon">⚡️</span>
-				<span class="workout__value">${workout.pace.toFixed(1)}</span>
-				<span class="workout__unit">min/km</span>
-			</div>
-			<div class="workout__details">
-				<span class="workout__icon">🦶🏼</span>
-				<span class="workout__value">${workout.cadence}</span>
-				<span class="workout__unit">spm</span>
-			</div>
-		</li>
-		`;
-
-		if (workout.type === 'cycling')
-		html += `
-			<div class="workout__details">
-				<span class="workout__icon">⚡️</span>
-				<span class="workout__value">${workout.speed}</span>
-				<span class="workout__unit">km/h</span>
-			</div>
-			<div class="workout__details">
-				<span class="workout__icon">⛰</span>
-				<span class="workout__value">${workout.elevGain}</span>
-				<span class="workout__unit">m</span>
-			</div>
-		</li>
-		`;
-
-		form.insertAdjacentHTML('afterend', html);
-	}
-
 	function setDescription(type, date) {
 		const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
@@ -180,7 +107,6 @@ export default async function workoutForm(map) {
 		description = setDescription(type, date);
 		const running = {description, pace, coordinates, distance, duration, cadence, date, id, type};
 		return running;
-
 	}
 
 	function cyclingWorkout(coordinates, distance, duration, elevGain, date, id) {
@@ -215,7 +141,6 @@ export default async function workoutForm(map) {
 
 
 	async function sendWorkoutToSanity(workout) {
-
 		try {
 			const mutations = [
 				{
