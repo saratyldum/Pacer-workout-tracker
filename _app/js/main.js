11d7e8102d5756ea92362679894d80a948ea7f1e
saratyldum <98975412+saratyldum@users.noreply.github.com>
2023-05-02 11:32:30 +0200

    created updateUI function

--- _app/js/main.js
+++ _app/js/main.js
@@ -6,6 +6,7 @@ import weeklyProgress from "./modules/weeklyProgress.js";
 import dailyProgress from "./modules/dailyProgress.js";
 import renderWorkouts from "./modules/renderWorkouts.js"
 import totalProgress from "./modules/totalProgress.js";
+import updateUI from "./modules/updateUI.js";
 
 // mapbox(); få denne til å kjøre herfra, ikke fra userlocation
 
@@ -16,8 +17,9 @@ if(navigator.geolocation)
 navigator.geolocation.getCurrentPosition(handleGeolocationSucess, errorPosition)
 
 function handleGeolocationSucess(position) {
-const map = loadMap(position)
-renderWorkouts(map, workouts);
+const map = loadMap(position);
+updateUI(map, workouts);
+
 workoutForm(map)
 
 }
@@ -25,11 +27,3 @@ workoutForm(map)
 function errorPosition() {
 alert('Could not get your position')
 }
-
-
-
-// getUserPosition();
-// workoutForm(); //return distance whenever a new workout is added
-weeklyProgress(); //use distance to add onto progress bar
-totalProgress();
-// dailyProgress();
---
+++ _app/js/modules/updateUI.js
@@ -0,0 +1,17 @@
+import renderWorkouts from "./renderWorkouts.js";
+import weeklyProgress from "./weeklyProgress.js";
+import totalProgress from "./totalProgress.js";
+import fetchWorkouts from "./fetchWorkouts.js";
+
+export default async function updateUI(map) {
+	const workouts = await fetchWorkouts();
+	await renderHTMl(map, workouts)
+
+	
+	async function renderHTMl(map, workouts) {
+		renderWorkouts(map, workouts);
+		await weeklyProgress();
+		await totalProgress();
+
+	}
+}
