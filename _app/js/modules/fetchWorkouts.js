95be1455767e36463306f10a9362e38b30e00324
saratyldum <98975412+saratyldum@users.noreply.github.com>
2023-05-03 13:09:10 +0200

    added simple try/catch for main sanity fetch

--- _app/js/modules/fetchWorkouts.js
+++ _app/js/modules/fetchWorkouts.js
@@ -1,20 +1,27 @@
 import { sanity } from "../sanity.js";
 
 export default async function fetchWorkouts() {
-	const query = `*[_type == 'workout'] {
-		_id,
-		coordinates,
-		date,
-		description,
-		distance,
-		duration,
-		elevGain,
-		cadence,
-		pace,
-		speed,
-		type,
-	 }`
+	let workouts; 
+	try {
+		const query = `*[_type == 'workout'] {
+			_id,
+			coordinates,
+			date,
+			description,
+			distance,
+			duration,
+			elevGain,
+			cadence,
+			pace,
+			speed,
+			type,
+		}`
+		
+		const workouts = await sanity.fetch(query);
+		return workouts
+	} catch(error) {
+		console.error(error.message);
+	}
 
-	const workouts = await sanity.fetch(query);
 	return workouts;
 }
