4eeae1858787b186726b9514a7690f55fd924733
saratyldum <98975412+saratyldum@users.noreply.github.com>
2023-05-03 11:55:13 +0200

    made it possible to delete workouts from list

--- _app/js/modules/updateUI.js
+++ _app/js/modules/updateUI.js
@@ -3,10 +3,10 @@ import weeklyProgress from "./weeklyProgress.js";
 import totalProgress from "./totalProgress.js";
 import fetchWorkouts from "./fetchWorkouts.js";
 import dailyProgress from "./dailyProgress.js"
+import deleteWorkout from "./deleteWorkout.js";
 
 export default async function updateUI(map, workout) {
 	// const workouts = await fetchWorkouts();
-	// console.log(workouts);
 	await renderHTMl(map, workout)
 	
 	async function renderHTMl(map, workout) {
@@ -14,6 +14,6 @@ export default async function updateUI(map, workout) {
 		await weeklyProgress();
 		await totalProgress();
 		await dailyProgress();
-
+		await deleteWorkout();
 	}
 }
