d32ed72d79b5e0274b9749e817c1db37f218eb18
saratyldum <98975412+saratyldum@users.noreply.github.com>
2023-05-04 12:15:00 +0200

    created render function to render user greeting

--- _app/js/modules/generateUserGreeting.js
+++ _app/js/modules/generateUserGreeting.js
@@ -1,8 +1,7 @@
 export default function generateUserGreeting(userInfo) {
-	console.log(userInfo);
 	const userContainer = document.querySelector('.user');
-
-	console.log(userContainer);
+	
+renderHTML()
 
 	function formatUserName(userInfo) {
 		const userName = userInfo.userName;
@@ -13,19 +12,24 @@ export default function generateUserGreeting(userInfo) {
 	}
 
 	function createUserDOMElement(firstName) {
-	const userImage = document.createElement('img');
-	const userMessage = document.createElement('div');
-
-	userImage.classList = 'user__image';
-	userImage.setAttribute('src', userInfo.userImage);
-	userImage.setAttribute('alt', 'User Profile Image');
-
-	userMessage.classList = 'user__message';
+		const userImage = document.createElement('img');
+		const userMessage = document.createElement('div');
+	
+		userImage.classList = 'user__image';
+		userImage.setAttribute('src', userInfo.userImage);
+		userImage.setAttribute('alt', 'User Profile Image');
+	
+		userMessage.classList = 'user__message';
 		userMessage.innerHTML = `Hello, <strong>${firstName}</strong>`;
 
 		return [userImage, userMessage]
 	
 	}
 
-	userContainer.append(userImage, userMessage)
+	function renderHTML() {
+		const firstName = formatUserName(userInfo);
+		const [userImage, userMessage] = createUserDOMElement(firstName);
+
+		userContainer.append(userImage, userMessage)
+	}
 }
