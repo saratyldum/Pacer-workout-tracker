b7c8d2fc9431b3d7532fe2d554f39ce75c539896
saratyldum <98975412+saratyldum@users.noreply.github.com>
2023-05-10 13:25:45 +0200

    deployed sanity studio

---
+++ _studio/dist/static/index-be436b57-c79d9c0c.js
@@ -0,0 +1,1 @@
+import{DocumentPaneProvider as m,DocumentPane as a}from"./index-56aa5cc3-0d7e222d.js";import"./sanity-d290c8c8.js";import"./index-62a03214.js";export{m as DocumentPaneProvider,a as default};
