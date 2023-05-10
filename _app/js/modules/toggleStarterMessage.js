87761231380eea6cf08d28d9763f2fc9f99e7a8f
saratyldum <98975412+saratyldum@users.noreply.github.com>
2023-05-10 11:39:27 +0200

    added comment to the starter message module

--- _app/js/modules/toggleStarterMessage.js
+++ _app/js/modules/toggleStarterMessage.js
@@ -1,3 +1,9 @@
+/**
+ * Shows a message if there are no workouts to be rendered to let the user know how to get started
+ * with tracking their workouts.
+ * 
+ * @param {boolean} state true or false depending if there are workouts to be rendered or not
+ */
 export default function toggleStarterMessage(state) {
 	const starterMessageContainer = document.querySelector('.addWorkoutMessage');
 	const starterMessage = state;
