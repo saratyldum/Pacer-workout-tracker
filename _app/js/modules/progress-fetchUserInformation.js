2bc4f4e5374577d55e6dc980906c29fb2c72a817
saratyldum <98975412+saratyldum@users.noreply.github.com>
2023-05-10 10:38:19 +0200

    added comments to the module fetching user info

--- _app/js/modules/progress-fetchUserInformation.js
+++ _app/js/modules/progress-fetchUserInformation.js
@@ -1,8 +1,13 @@
 import { sanity } from "../sanity.js";
 import handleError from "./handleError.js";
 
+/**
+ * Fetches the user information and image from the Sanity Studio.
+ * @returns the user
+ */
 export default async function progressFetchUserInformation() {
 	let user;
+	
 	try {
 		const query = `*[_type == 'user'][0] {
 			userName,
