1606807bf98a7952ca5e07f83108cda597f60f72
saratyldum <98975412+saratyldum@users.noreply.github.com>
2023-05-22 13:27:08 +0200

    refactored handle error module

--- _app/js/modules/handleError.js
+++ _app/js/modules/handleError.js
@@ -10,24 +10,34 @@ export default function handleError(errorMessage) {
 	const closeButton = document.querySelector('.error-box__close-button')
 	const header = document.querySelector('.header');
 	const logo = document.querySelector('.logo');
+	let isVisible = false;
 
 	showErrorMessage(errorMessage)
 
 	closeButton.addEventListener('click', removeErrorMessage)
 
 	function showErrorMessage(errorMessage) {
-		errorContainer.style.display = 'flex';
-		errorMessageContainer.textContent = errorMessage;
-		header.classList.add('error');
-		logo.style.display = "none";
-		closeButton.focus();
+		isVisible = true;
+		renderHTMl(isVisible)
 	}
 
 	function removeErrorMessage() {
-		errorContainer.style.display = 'none';
-		header.classList.remove('error');
-		logo.style.display = "block";
+		isVisible = false;
+		renderHTMl(isVisible)
+	}
 
+	function renderHTMl(isVisible) {
+		if (isVisible) {
+			errorContainer.style.display = 'flex';
+			errorMessageContainer.textContent = errorMessage;
+			header.classList.add('error');
+			logo.style.display = "none";
+			closeButton.focus();
+		} else {
+			errorContainer.style.display = 'none';
+			header.classList.remove('error');
+			logo.style.display = "block";
+		}
 	}
 
 }
